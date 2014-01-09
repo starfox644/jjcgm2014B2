bed115951600dcbdbac7e7f5b0430b52d1ea6fdd
galdween <galdween@NachosProject>
2014-01-09 15:18:24 +0100

    ajout du test putStringNumber dans le dossier etape2 des tests avec la bonne version

---
+++ test/etape2/putstringNumber.c
@@ -0,0 +1,9 @@
+#include "syscall.h"
+
+int main()
+{
+	// test pour voir si les chiffres sont bien pris en compte et surtout le -1 et après
+	PutString("123456789\n");
+	PutString("10 11 12 -2 -3 -1 25 30\n");
+    Halt();
+}
