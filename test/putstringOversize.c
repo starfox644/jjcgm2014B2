97ca49d09db6af3746c12d9b315a20d78340a9ed
Sarkal <cano.gregory@gmail.com>
2014-01-09 14:38:35 +0100

    Ajustement du test putstringOversize.c (à 1024 char)

--- test/putstringOversize.c
+++ test/putstringOversize.c
@@ -2,7 +2,12 @@
 
 int main()
 {
+	int i, max = MAX_STRING_SIZE+10;
+	char s[max];
+	for (i=0; i < max-1; i++)
+		s[i] = 'a'+ (i%26);
+	s[i+1] = '\0';
 	// Test with oversize string
-	PutString("abcdefghijklmnopqrstuvwxyz\n");
-    Halt();
+	PutString(s);
+    return 0;
 }
