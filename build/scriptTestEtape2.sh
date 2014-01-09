592c98cfc03a943f9c1984be6cd29fbff02e95bb
galdween <galdween@NachosProject>
2014-01-09 17:43:40 +0100

    correction du script des tests on ne tente plus de lancer les *.o on ne tente plus de lancer le dossier ou les programmes de test ce trouve. correction de l'affichage qui prend en compte les \n.

--- build/scriptTestEtape2.sh
+++ build/scriptTestEtape2.sh
@@ -7,19 +7,26 @@ PWD=`pwd`
 #on rajoute notre dossier d'étape correspondant
 directory=$PWD/etape2/ 
 #on lance notre recherche pour avoir les noms des fichiers executable
-
- 
+command=`find $directory -executable` 
+#on efface les .o pour eviter des problèmes.
+echo `rm $directory/*.o`
 #Pour chaque programme executable existant dans notre recherche
 
-for files in $directory/`find -executable`
+for files in *$command*
 do
-	#on affiche le nom du programme et on lance le programme
-	echo ''
-	echo '**********************************************'
-	echo '* lancement du programme de test :' $files  '*'
-	echo '**********************************************'
-	echo ''	
-	echo `./nachos-step2 -x $files`
-	echo ''
+	if [ '$files' != $directory ]
+	then
+		#on affiche le nom du programme et on lance le programme
+		echo ''
+		echo '**********************************************'
+		echo '* lancement du programme de test :' $files  '*'
+		echo '**********************************************'
+		echo ''	
+		./nachos-step2 -x $files > /tmp/resultat_test_etape2
+		cat /tmp/resultat_test_etape2
+		echo ''
+	fi
 done
+
+
 
