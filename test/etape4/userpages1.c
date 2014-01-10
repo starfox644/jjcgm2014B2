7afafdc1a5ba3d24738333e42cc7b4451e0295b8
galdween <galdween@NachosProject>
2014-01-10 05:43:30 +0100

    Rajout de programme de test pour l'étape 4 ProcPutString qui lance putstring dans un autre processus userpages1.c creer des threads

---
+++ test/etape4/userpages1.c
@@ -0,0 +1,34 @@
+/*
+ * userpages1.c
+ *
+ *  Created on: 10 janv. 2014
+ *      Author: galdween
+ */
+#include "syscall.h"
+#define THIS "aaa"
+#define THAT "bbb"
+
+const int N = 10; //Choose it large enough!
+
+void puts(char*s){
+	char*p;
+	for(p = s; *p != '\0'; p++){
+		PutChar(*p);
+	}
+}
+
+void f(void *s){
+	int i;
+	for(i = 0; i < N; i++){
+		puts((char*)s);
+	}
+}
+
+int main(){
+	int i;
+	UserThreadCreate(f, (void *) THIS);
+	f((void *) THAT);
+
+	return 0;
+}
+
