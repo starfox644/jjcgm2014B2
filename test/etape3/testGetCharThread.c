9e3ebe3c1c806c81e2cc01266215836eec8a7c6e
Sarkal <cano.gregory@gmail.com>
2014-01-10 19:25:25 +0100

    testGetCharThread demande un char a l'utilisateur et l'affiche dans un thread créé ensuite. Ajout de commentaires pour testGetCharMultiThread

--- test/etape3/testGetCharThread.c
+++ test/etape3/testGetCharThread.c
@@ -1,41 +1,32 @@
 /**
  * Ce programme demande un caractere a l'utilisateur, cree un thread
  * en le lui donnant en parametre. Le thread cree affiche le caractere
- * a l'ecran et termine. Des appels a PutChar concurentiels sont effectues
- * pour tester la synchronisation
+ * a l'ecran et termine.
  */
-
 
 #include "syscall.h"
 
 typedef struct Arg {
-	int  wait;
+	volatile int  wait;
 	char a;
 } Arg;
 
 void f(void* arg)
 {
-	PutChar('5');
 	Arg *param = (Arg*) arg;
 	PutChar(param->a);
-	PutChar('6');
 	param->wait = 0;
-	PutChar('7');
 	UserThreadExit();
 }
 
 int main()
 {
 	Arg arg;
-	PutChar('1');
+	PutString("Entrez un caractere :");
 	arg.wait = 1;
-	PutChar('2');
 	arg.a = GetChar();
-	PutChar('3');
 	UserThreadCreate(f, &arg);
-	PutChar('4');
-	while(arg.wait)
-		PutChar('');
-	PutChar('8');
+	while(arg.wait);
+	PutChar('\n');
     return 0;
 }
