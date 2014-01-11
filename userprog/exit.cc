3136c71375f6a4c87cfa18930e8bc7c9efe13651
jeremy <jeremy.wambecke@gmail.com>
2014-01-11 15:35:38 +0100

    revue des commentaires de la synchronisation de la fin des threads

--- userprog/exit.cc
+++ userprog/exit.cc
@@ -4,17 +4,22 @@
 
 void do_exit(int returnCode)
 {
-	printf("Program stopped with return code : %d\n", returnCode);
-	DEBUG('a',"Program exit");
+	// indicates that the main thread is waiting for the others
 	currentThread->space->s_nbThreads->P();
 	currentThread->space->attente = true;
 	currentThread->space->s_nbThreads->V();
 	while(currentThread->space->getNbThreads() > 0)
 	{
+		// semaphore wait for waiting the others threads
 		currentThread->space->s_exit->P();
 	}
+
 	if(currentThread->space != NULL)
 		delete currentThread->space;
+
+	printf("Program stopped with return code : %d\n", returnCode);
+	DEBUG('a',"Program exit");
+
 	// stop the program
 	interrupt->Halt ();
 }
