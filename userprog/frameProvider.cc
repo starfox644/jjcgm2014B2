4a7b381f6316f1eb56fe39c61cd0e67dc472edc2
carole <carole.plasson@gmail.com>
2014-01-13 17:22:42 +0100

    Ajout classe FrameProvider

--- userprog/frameProvider.cc
+++ userprog/frameProvider.cc
@@ -1,5 +1,39 @@
 #ifdef step4
-#include "FrameProvider.h"
+#include "frameProvider.h"
 
+
+FrameProvider::FrameProvider ()
+{
+	bitmap = new BitMap(NumPhysPages);
+}
+
+int FrameProvider::GetEmptyFrame()
+{
+	int indexFrame;
+	if ((indexFrame = bitmap->Find ()) == -1)
+	{
+		printf(" No frame are free ");
+		return -1;
+	}
+	// initialisation du cadre à 0
+	bzero (machine->mainMemory[indexFrame*PageSize], PageSize);
+	// indique que la page d’index “indexFrame” est occupee
+	bitmap->mark(indexFrame);
+	return (indexFrame * PageSize);
+}
+
+void FrameProvider::ReleaseFrame(int addrFrame)
+{
+	int index = addrFrame / PageSize;
+	// indique que la page est libre
+	bitmap->clear(index);
+}
+
+
+int FrameProvider::NumAvailFrame()
+{
+	// retourne le nombre de cadres libres
+	return bitmap->NumClear();
+}
 
 #endif
--- userprog/frameProvider.h
+++ userprog/frameProvider.h
@@ -1,5 +1,15 @@
 #ifdef step4
 #include "bitmap.h"
 
+class FrameProvider
+{
+  public:
+    FrameProvider ();	    // create a frame provider
+    int GetEmptyFrame();  	// renvoie l’adresse physique du premier cadre libre (apres l’avoir initialise) ou -1 s’il n’y en a plus
+    void ReleaseFrame(int addrFrame);  // libère la page d’adresse addrFrame
+    int NumAvailFrame()	    // renvoie le nombre de cadres disponibles
 
+  private:
+    BitMap *bitmap;
+}
 #endif
