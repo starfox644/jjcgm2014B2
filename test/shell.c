0e38aa58b2dc7d3d5605977568ea326e8805816e
galdween <zoomgengen@hotmail.com>
2014-01-17 08:54:51 +0100

    rajout de shell.c dans test pour éviter une erreur de compile

---
+++ test/shell.c
@@ -0,0 +1,19 @@
+/*
+ * Programme utilisateur de shell pour que les utilisateurs ai un interface.
+ */
+
+/*int main (){
+
+
+	char * in, out, buffer[128];
+
+    while (1){
+    	PutString("<Nachos_Shell>");	//affichage du prompt
+    	do{// tant que l'on appuie pas sur entrée on lis les caractères entrés dans la console.
+    		buffer[i] = GetChar();	//on ajoute les caractères lu au buffer
+    	}while (buffer[i++] != '\n');
+    	buffer[--i] = '\0';
+    	//on a finis de lire la commande entrée a la console
+    	eval(buffer);
+    }
+}*/
