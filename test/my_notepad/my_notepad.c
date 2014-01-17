0446a97f363a260d029d4b68c761a2e465c3a020
galdween <zoomgengen@hotmail.com>
2014-01-17 18:26:18 +0100

    Ajout des fichiers pour my_notepad modification du makefile pour le compiler

---
+++ test/my_notepad/my_notepad.c
@@ -0,0 +1,10 @@
+/*
+ * my_notepad.c
+ *
+ *  Created on: 17 janv. 2014
+ *      Author: galdween
+ */
+
+
+
+
---
+++ test/my_notepad/my_notepad.h
@@ -0,0 +1,17 @@
+/*
+ * my_notepad.h
+ *
+ *  Created on: 17 janv. 2014
+ *      Author: galdween
+ */
+
+#ifndef MY_NOTEPAD_H_
+#define MY_NOTEPAD_H_
+#include "../nachos_libc/String.h"
+#include "../nachos_libc/Printf.h"
+#include "system.h"
+
+
+
+
+#endif /* MY_NOTEPAD_H_ */
