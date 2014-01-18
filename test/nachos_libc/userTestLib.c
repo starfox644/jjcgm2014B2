ad418d63c266637c7b4ad98df103806665ea88e0
galdween <zoomgengen@hotmail.com>
2014-01-18 05:43:39 +0100

    ajout d'un test pour vérifier le fonctionnement de Printf et de scanf

---
+++ test/nachos_libc/userTestLib.c
@@ -0,0 +1,28 @@
+/*
+ * userTestLib.c
+ *
+ *  Created on: 18 janv. 2014
+ *      Author: galdween
+ */
+#include "Printf.h"
+
+int main(){
+
+	int d;
+	char c;
+	char* s;
+	Printf("Veuillez entrer un entier svp ?\n", 0);
+	Scanf("%d", &d);
+	Printf("Entier entré : %d\n",d);
+	Printf("Veuillez entrer un char svp ?\n", 0);
+	Scanf("%c", &c);
+	Printf("Char entré : %c\n",c);
+	Printf("Veuillez entrer un string svp ?\n", 0);
+	Scanf("%s", &s);
+	Printf("String entré : %s\n",s);
+
+	retun 0;
+}
+
+
+
