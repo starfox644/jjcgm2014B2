33b42f0108c18e99b77590c5a08fdf52b154cc39
Sarkal <cano.gregory@gmail.com>
2014-01-20 13:44:33 +0100

    Gestion double attente sur meme processus + ajout de test waitpid.

---
+++ test/etape4/progWait.c
@@ -0,0 +1,14 @@
+/*
+ * 	progWait.c
+ *	Lance une boucle pour attendre un certain temps afin de tester le waitpid via waitPidMulti
+ *  Created on: 17 janv. 2014
+ *      Author: Sarkal
+ */
+#include "syscall.h"
+
+int main(){
+	int i;
+	for(i = 0; i < 50000; i++);
+	return 0;
+}
+
