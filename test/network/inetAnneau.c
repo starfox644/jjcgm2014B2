49bcc7ec00b91f61e8c5d63fc50eb2d99f54b771
galdween <zoomgengen@hotmail.com>
2014-01-22 13:02:21 +0100

    ajout d'instruction en début de test pour l'execution

--- test/network/inetAnneau.c
+++ test/network/inetAnneau.c
@@ -16,15 +16,18 @@ int main(){
 	Printf("\n-----------------------------------------\n");
 	Printf("Lancement du test inetAnneau : \n");
 	Printf("envoi un message a une machine qui l'envoi a la suivante\n");
+	Printf("Il faut que la première machine porte le numéro 0,\n");
+	Printf("et la dernière machine doit envoyer un message vers la première machine donc 0 \n");
+	Printf("Exemple machine depart : ./nachos -m 0 -o X -x ./network/inetAnneau\n");
+	Printf("\t\t\t...\n");
+	Printf("Exemple machine de fin : ./nachos -m X -o 0 -x ./network/inetAnneau\n");
 	Printf("-----------------------------------------\n");
-	int to = 0,from = 1;
+	int to = 0;
 	char* message = (char*) malloc(10 * sizeof(char));
 	char* reception = (char*) malloc(10 * sizeof(char));
 	sock_t SocketEnvoi, SocketReception;
 
 	//demande a l'utilisateur les informations pour créer la socket
-	Printf("FROM : \n");
-	Scanf("%d",&from);
 	Printf("TO :\n");
 	Scanf("%d",&to);
 	Sleep(5);
@@ -35,9 +38,9 @@ int main(){
 
 
 	if(GetHostname() == 0){
-		message = Itoa(from); //on donne le numero de la machine de départ
+		message = Itoa(GetHostname()); //on donne le numero de la machine de départ
 		if(envoyerMessage(SocketEnvoi,message) ==-1){
-			Printf("Erreur d'envoi de Message depuis la machine %d\n",from);
+			Printf("Erreur d'envoi de Message depuis la machine %d\n",GetHostname());
 		}
 
 		if(recevoirMessage(SocketReception,reception) == -1){
@@ -49,10 +52,10 @@ int main(){
 		}
 		StrCpy(reception,message);
 		if(envoyerMessage(SocketEnvoi,message) ==-1){
-			Printf("Erreur d'envoi de Message depuis la machine %d\n",from);
+			Printf("Erreur d'envoi de Message depuis la machine %d\n",GetHostname());
 		}
 	}
-	if(Atoi(reception) == from){	//si le message recu correspond au numéro d'hote alors on a fait la boucle
+	if(Atoi(reception) == GetHostname()){	//si le message recu correspond au numéro d'hote alors on a fait la boucle
 		Printf("\nMessage reçu : %s\n",reception);
 		Printf("\nboucle faite\n");
 	}else{
